d86c377f08b6d47bc2107aca6fbe9a40381c6b57
Roberto Rojo <rrojo.va@gmail.com>
2022-12-27 04:49:36 +0100

    RecyclerView done

--- app/src/main/java/com/advanced/advanceddragonball/HeroListFragment.kt
+++ app/src/main/java/com/advanced/advanceddragonball/HeroListFragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.advanced.advanceddragonball.databinding.FragmentHeroListBinding
+import java.util.UUID
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class HeroListFragment : Fragment() {
 
     private var _binding: FragmentHeroListBinding? = null
@@ -19,7 +17,7 @@ class HeroListFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    private var hero = listOf<String>()
+    private val adapter = HeroListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,11 +31,25 @@ class HeroListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        with(binding) {
+            heroList.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            heroList.adapter = adapter
+            adapter.submitList(getHeroes(1000))
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getHeroes(size: Int): List<Hero> {
+        val heroes = mutableListOf<Hero>()
+
+        for (i in 0 .. size) {
+            heroes.add(Hero("Hero $i",UUID.randomUUID().toString()))
+        }
+
+        return heroes
     }
 }
